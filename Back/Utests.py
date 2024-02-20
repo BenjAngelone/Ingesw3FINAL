54993eacc2ed45cb30882172ea854b91319956f0
Benjamin Angelone <80827906+BenjAngelone@users.noreply.github.com>
2024-02-20 15:58:46 -0300

    Update Utests.py

--- Back/Utests.py
+++ Back/Utests.py
@@ -1,17 +1,15 @@
 import unittest
+from flask import jsonify
+from backend import app
 
 class TestBackend(unittest.TestCase):
+    def test_test_backend(self):
+        with app.test_client() as client:
+            response = client.get('/test')
+            data = response.get_json()
 
-    def test_endpoint_test(self):
-        # Se envía una solicitud GET al endpoint de prueba
-        response = app.test_client().get('/test')
-
-        # Se verifica el código de respuesta
-        self.assertEqual(response.status_code, 200)
-
-        # Se verifica el contenido de la respuesta
-        data = response.get_json()
-        self.assertEqual(data['respuesta'], "Backend funcionando correctamente")
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(data["respuesta"], "Ok")
 
 if __name__ == '__main__':
     unittest.main()
