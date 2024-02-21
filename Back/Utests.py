781a7d1fe6233f28d5172744a5b5cb240ea1a9b9
Benjamin Angelone <80827906+BenjAngelone@users.noreply.github.com>
2024-02-21 10:38:08 -0300

    Update Utests.py

--- Back/Utests.py
+++ Back/Utests.py
@@ -1,6 +1,6 @@
 import unittest
 from flask import jsonify
-from backend import app, conectar_a_bd, contar_frecuencias
+from backend import app
 
 class TestBackend(unittest.TestCase):
     
@@ -10,34 +10,14 @@ class TestBackend(unittest.TestCase):
     def tearDown(self):
         pass
         
-    def test_recibir_texto(self):
-        # Mock de la solicitud de entrada
-        response = self.app.post('/backend', json={'texto': 'test'})
-        self.assertEqual(response.status_code, 200)
-                
-    def test_contar_frecuencias(self):
-        # Mock de la conexión a la base de datos
-        mock_conexion_db = MagicMock()
-        mock_cursor = MagicMock()
-        mock_cursor.fetchall.return_value = [('test', 2), ('example', 3)]
-        mock_conexion_db.cursor.return_value = mock_cursor
-        
-        # La función conectar_a_bd se usa indirectamente dentro de contar_frecuencias
-        frecuencias = contar_frecuencias(mock_conexion_db)
-        
-        self.assertEqual(len(frecuencias), 2)
-        self.assertEqual(frecuencias[0]['palabra'], 'test')
-        self.assertEqual(frecuencias[0]['frecuencia'], 2)
-        self.assertEqual(frecuencias[1]['palabra'], 'example')
-        self.assertEqual(frecuencias[1]['frecuencia'], 3)
-        
     def test_test_backend(self):
         with app.test_client() as client:
-            response = client.get('/test')
+            response = client.post('/test', json={"texto": "hello"})
             data = response.get_json()
 
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(data["respuesta"], "Ok")
+            self.assertEqual(data["palabra_original"], "hello")
+            self.assertEqual(data["palabra_en_espejo"], "olleh")
 
 if __name__ == '__main__':
     unittest.main()
